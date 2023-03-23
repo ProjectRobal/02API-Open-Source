a5f387c349cce8361fa7f86fb8381d2123e72e4e
ProjectRobal <patryk1-70@wp.pl>
2023-03-23 11:11:57 +0100

    I have added help command to build script

--- dev.sh
+++ dev.sh
@@ -1,42 +1,63 @@
 #!/bin/bash
 
-if [ "$1" = "build" ]; then
+cmd="help"
+
+if [ $# -ge 1 ]
+  then
+    cmd=$1
+fi
+
+if [ $cmd = "build" ]; then
 
     docker compose build
 
-elif [ "$1" = "up" ]; then
+elif [ $cmd = "up" ]; then
 
     docker compose up
 
-elif [ "$1" = "down" ]; then
+elif [ $cmd = "down" ]; then
 
     docker compose down
 
-elif [ "$1" = "start "]; then
+elif [ $cmd = "start" ]; then
 
     docker compose start
 
-elif [ "$1" = "stop "]; then
+elif [ $cmd = "stop" ]; then
 
     docker compose stop
 
-elif [ "$1" = "run" ]; then
+elif [ $cmd = "run" ]; then
 
     docker compose build
     docker compose up
 
-elif [ "$1" = "purge" ]; then
+elif [ $cmd = "purge" ]; then
 
     docker compose rm -f -s -v
     rm -R  sql/data
     rm -R mqtt/data
 
-elif [ "$1" = "debug" ]; then
+elif [ $cmd = "debug" ]; then
 
     docker compose run -i web bash
 
-elif [ "$1" = "init_root" ]; then
+elif [ $cmd = "init_root" ]; then
 
     docker compose run -T web python manage.py createsuperuser --noinput
+
+
+elif [ $cmd = "help" ]; then
+
+echo "build - zbuduj kontenery"
+echo "up - uruchom i utwórz kontenery"
+echo "down - zatrzymaj i usuń kontenery"
+echo "start - uruchom kontenery"
+echo "stop - zatrzymaj kontenery"
+echo "run - zbuduj i uruchom kontenery"
+echo "purge - usuń kontenery wraz z ich danymi"
+echo "debug - uruchom terminal bash na kontenerze django"
+echo "init_root - utwórz użytkownika roota w django"
+echo "help - komenda pomoc"
 
 fi
