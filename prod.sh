6324beacd72a64b978c14cb2fbd0e2fdd068df55
ProjectRobal <patryk1-70@wp.pl>
2023-12-12 19:23:42 +0100

    New style to prod

--- prod.sh
+++ prod.sh
@@ -46,7 +46,7 @@ elif [ $cmd = "stop" ]; then
 elif [ $cmd = "run" ]; then
 
     docker compose -f compose.yml cp ../../django/src/. web:/app
-    docker compose -f compose.yml -d up
+    docker compose -f compose.yml up -d
 
 elif [ $cmd = "purge" ]; then
 
