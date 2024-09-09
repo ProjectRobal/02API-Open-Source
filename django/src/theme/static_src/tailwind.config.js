28b1c3da8f795952a0eb50d9a232bb85de477533
ProjectRobal <patryk1-70@wp.pl>
2024-09-09 14:17:25 +0200

    A new look for CyberLej ranking

--- django/src/theme/static_src/tailwind.config.js
+++ django/src/theme/static_src/tailwind.config.js
@@ -45,7 +45,86 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage:
+            {
+                "synthwave":"url(/static/vecteezy_80s.gif)"
+            },
+            animation:
+            {
+                "first_stage":"first_place 3s linear infinite",
+                "second_stage":"second_place 3s linear infinite",
+                "third_stage":"third_place 3s linear infinite",
+                "ranking_title":"title 4s linear infinite"
+            },
+            keyframes:
+            {
+                title:{
+                    "0%,50%":{
+                        color:"#ff78f8"
+                    },
+                    "20%,80%":
+                    {
+                        color:"#78d9ff"
+                    },
+                    "10%,60%":
+                    {
+                        color:"#ff78bc"
+                    },
+                    "5%,30%":
+                    {
+                        color:"#ff7878"
+                    },
+                    "45%,85%":
+                    {
+                        color:"#fffd78d3"
+                    },
+                    "75%,100%":
+                    {
+                        color:"#78ff83"
+                    }
+                },
+                first_place:{
+                    "0%":{
+                        color:"#e478ff"
+                    },
+                    "50%":
+                    {
+                        color:"#ff78f8"
+                    },
+                    "100%":
+                    {
+                        color:"#ff78b7"
+                    }
+                },
+                second_place:{
+                    "0%":{
+                        color:"#d0ff78"
+                    },
+                    "50%":
+                    {
+                        color:"#8fff78"
+                    },
+                    "100%":
+                    {
+                        color:"#78ffac"
+                    }
+                },
+                third_place:{
+                    "0%":{
+                        color:"#78aeff"
+                    },
+                    "50%":
+                    {
+                        color:"#7898ff"
+                    },
+                    "100%":
+                    {
+                        color:"#ac78ff"
+                    }
+                }
+            }
+        },
         colors: {
             // Configure your color palette here
             primary:'#A66E62',
